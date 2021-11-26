02f57dd072247990eafb3b594bf398431cf99eba
itzaur <preneur85@gmail.com>
2021-11-26 15:14:40 +0300

    Demo commit

--- app.js
+++ app.js
@@ -118,7 +118,6 @@ const pages = {
 const nav = document.querySelector('.navigation');
 const navContainer = document.querySelector('.navigation__list');
 const navItems = document.querySelectorAll('.navigation__item');
-// const navItemActive = document.querySelector('.active');
 const navLinks = document.querySelectorAll('.navigation__link');
 const btnMenu = document.querySelector('.btn-toggle');
 const btnExplore = document.querySelector('.btn-explore');
@@ -147,54 +146,3 @@ btnMenu.addEventListener('click', () => {
     btnMenu.style.background = `no-repeat center/cover url('../assets/shared/icon-hamburger.svg')`;
   }
 });
-
-// const destinationNav = document.querySelector('.destination-nav');
-// const destinationStats = document.querySelector('.stats');
-// const planets = document.querySelectorAll('.destination-nav__list');
-// const planetName = document.querySelector('.home__title--destination');
-// const img = document.querySelector('.moon img');
-// const imgWebp = document.querySelector('.moon source');
-// const description = document.querySelector('.home__subtitle--destination');
-// const distance = document.querySelector('.stats__distance h3');
-// const travel = document.querySelector('.stats__time h3');
-
-// destinationNav.addEventListener('click', function (e) {
-//   const clicked = e.target.closest('.destination-nav__list');
-//   if (!clicked) return;
-//   planets.forEach(planet => planet.classList.remove('active'));
-
-//   clicked.classList.add('active');
-// });
-
-// planets.forEach(planet => {
-//   planet.addEventListener('click', e => {
-
-//     const tabID = e.target.parentNode.getAttribute('tabindex');
-//     const item = pages.destinations[tabID];
-
-//     if (!tabID) return;
-
-//     img.src = item.images.png;
-//     imgWebp.srcset = item.images.webp;
-//     description.textContent = item.description;
-//     distance.textContent = item.distance;
-//     travel.textContent = item.travel;
-//     planetName.textContent = item.name;
-
-//     animation();
-//   });
-// });
-
-// function animation() {
-//   img.classList.add('opacity');
-//   planetName.classList.add('destinationMove');
-//   description.classList.add('destinationMove');
-//   destinationStats.classList.add('destinationTravelMove');
-
-//   setTimeout(function () {
-//     img.classList.remove('opacity');
-//     planetName.classList.remove('destinationMove');
-//     description.classList.remove('destinationMove');
-//     destinationStats.classList.remove('destinationTravelMove');
-//   }, 2500);
-// }
--- technology.js
+++ technology.js
@@ -3,56 +3,53 @@
 const dotsTechnologyContainer = document.querySelector('.dots--technology');
 const dotsTechnology = document.querySelectorAll('.dots--technology div');
 const technologyTitle = document.querySelector('.technology-title');
-// const technologySup = document.querySelector('.home__suptitle--technology');
+
 const technologyName = document.querySelector('.home__title--technology');
 const technologyDescription = document.querySelector(
   '.home__subtitle--technology'
 );
-const technologyImg = document.querySelectorAll('.technology img');
-// const crewImgWebp = document.querySelector('.technology source');
+// const technologyImg = document.querySelectorAll('.technology img');
+const technologyImgPortrait = document.querySelector('.img-portrait');
+const technologyImgLandscape = document.querySelector('.img-landscape');
 
 dotsTechnologyContainer.addEventListener('click', e => {
-  //   const clickedSpan = e.target.getAttribute('data-num');
-  //   clickedSpan.setAttribute('data-num', false);
-
-  //   if (clickedSpan === 'true') {
-
-  //     let res = e.target.closest('.dots--technology div');
-  //     res.setAttribute('data-active', true);
-
-  //     console.log(res);
-  //   }
   dotsTechnology.forEach(dot => dot.classList.remove('active'));
   const dotId = e.target.getAttribute('tabindex');
-  // const clicked = e.target;
   const clicked = e.target.closest('.dots--technology div');
+  const item = pages.technology[dotId];
 
   clicked.classList.add('active');
 
-  const item = pages.technology[dotId];
-  //   crewTitle.textContent = item.role;
   technologyName.textContent = item.name;
   technologyDescription.textContent = item.description;
-  technologyImg.forEach(img => {
-    img.src = item.images.portrait;
-    img.src = item.images.landscape;
-  });
-  // technologyImg.src = item.images.portrait;
-  //   crewImgWebp.srcset = item.images.landscape;
+  // console.log(dotId);
+  // if (dotId === '0') {
+  //   console.log('yes');
+  // } else {
+  //   console.log('no');
+  // }
+  technologyImgPortrait.src = item.images.portrait;
+  technologyImgLandscape.src = item.images.landscape;
+  // technologyImg.forEach(img => {
+  //   img.src = item.images.portrait;
+  //   img.src = item.images.landscape;
+  // });
 
   animation();
 });
 
 function animation() {
-  technologyImg.forEach(img => img.classList.add('opacity'));
-  // technologyImg.classList.add('opacity');
+  technologyImgPortrait.classList.add('opacity');
+  technologyImgLandscape.classList.add('opacity');
+  // technologyImg.forEach(img => img.classList.add('opacity'));
   technologyTitle.classList.add('destinationMove');
   technologyName.classList.add('destinationMove');
   technologyDescription.classList.add('destinationMove');
 
   setTimeout(function () {
-    technologyImg.forEach(img => img.classList.remove('opacity'));
-    // technologyImg.classList.remove('opacity');
+    technologyImgPortrait.classList.remove('opacity');
+    technologyImgLandscape.classList.remove('opacity');
+    // technologyImg.forEach(img => img.classList.remove('opacity'));
     technologyTitle.classList.remove('destinationMove');
     technologyName.classList.remove('destinationMove');
     technologyDescription.classList.remove('destinationMove');
